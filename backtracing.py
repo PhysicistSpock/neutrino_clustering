Wrong shape in final plot

backtracing.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import draw
 from shared.preface import *
 import shared.my_units as my
 import shared.functions as fct
@@ -35,8 +36,7 @@ def EOMs(s, y, rho_0, M_vir):
 def backtrack_1_neutrino(y0_Nr):
     """Simulate trajectory of 1 neutrino."""
 
-    #! Redshift start, redshift to integrate back to, redshift amount of steps
-    z_start, z_stop, z_amount = 0, 0.5, 50
+    z_start, z_stop, z_amount = CC.Z_START, CC.Z_STOP, CC.Z_AMOUNT
 
     global z_steps, s_steps  # other functions can use these variables
 
@@ -79,22 +79,46 @@ if __name__ == '__main__':
     start = time.time()
 
     #! Amount of neutrinos to simulate
-    neutrinos = CC.NR_OF_NEUTRINOS
+    nu_Nr = CC.NR_OF_NEUTRINOS
 
     # Position of earth w.r.t Milky Way NFW halo center
     x1, x2, x3 = 8.5, 8.5, 0.
     x0 = np.array([x1, x2, x3])
-    
-    # Random draws for velocities
-    ui_min, ui_max = 2000.*unit.km/unit.s, 4000.*unit.km/unit.s
-    ui_min_kpc, ui_max_kpc = ui_min.to(my.Uunit), ui_max.to(my.Uunit)
-    ui = np.array([
-        np.random.default_rng().uniform(ui_min_kpc.value, ui_max_kpc.value, 3) 
-        for _ in range(neutrinos)
-        ])
+
+
+    def draw_ui(amount):
+        
+        '''
+        # conversion factor for limits
+        cf = 5.3442883e-28 / CC.NU_MASS.to(unit.kg, unit.mass_energy()).value
+        T_nu_eV = my.T_nu.to(unit.eV, unit.temperature_energy()).value
+        
+        # limits on velocity
+        lower = 0.01*T_nu_eV*cf
+        upper = 10*T_nu_eV*cf
+
+        #? very confusing, limits way too high
+        '''
+
+        #! quick guesstimate
+        lower = 2000
+        upper = 4000
+
+        # initial velocities array
+        ui_km = np.geomspace(lower, upper, amount)*unit.km/unit.s
+        ui_kpc = ui_km.to(unit.kpc/unit.s)
+
+        # We drew magnitude of velocity, assume u_x=u_y=u_z.
+        ui_array = np.array([np.ones(3)*(elem/np.sqrt(3)) for elem in ui_kpc])
+
+        return ui_array
+
+
+    # draw initial velocities
+    ui = draw_ui(nu_Nr)
 
     # Combine vectors and append neutrino particle number
-    y0_Nr = np.array([np.concatenate((x0,ui[i],[i+1])) for i in range(neutrinos)])
+    y0_Nr = np.array([np.concatenate((x0,ui[i],[i+1])) for i in range(nu_Nr)])
 
 
     Processes = 16

number_density.py
@@ -4,31 +4,45 @@ import shared.functions as fct
 import shared.control_center as CC
 
 
+def number_density_1_mass(m_nu_eV):
+
+    # Amount of simulated neutrinos
+    Ns = np.arange(CC.NR_OF_NEUTRINOS, dtype=int)
+
+    # load initial and final velocity
+    u0 = [np.load(f'neutrino_vectors/nu_{Nr+1}.npy')[0][3:6] for Nr in Ns]
+    u1 = [np.load(f'neutrino_vectors/nu_{Nr+1}.npy')[-1][3:6] for Nr in Ns]
+
+    # intermediate arrays
+    a0 = np.array([np.sqrt(np.sum(u**2)) for u in np.array(u0)])
+    a1 = np.array([np.sqrt(np.sum(u**2)) for u in np.array(u1)])
+
+    # convert mass(es) from eV to kg
+    m_nu_kg = m_nu_eV.to(unit.kg, unit.mass_energy())
+
+    n_nus = np.zeros(len(m_nu_kg))
+    for i, m in enumerate(m_nu_kg.value):
+
+        # convert velocities to momenta
+        p0, p1 = a0 * m, a1 * m
+
+        #NOTE: number_density function need input momenta in units [kg*kpc/s]
+        p_unit = unit.kg*unit.kpc/unit.s
+        n_nus[i] = fct.number_density(p0*p_unit, p1*p_unit).value
+
+    np.save('neutrino_data/number_densities.npy', n_nus)
+
+
 if __name__ == '__main__':
 
-    neutrinos = 10000
+    # 10 to 300 meV like in the paper
+    mass_range_eV = np.linspace(0.01, 0.3, 100) * unit.eV
 
-    # neutrino mass
-    m_nu = 0.05 * unit.eV  # in natural units
-    m_nu_kg = m_nu.to(unit.kg, unit.mass_energy())  # in SI units
+    number_density_1_mass(mass_range_eV)
 
-    p0s, p_backs = np.zeros(neutrinos), np.zeros(neutrinos)
-    for Nr in range(neutrinos):
-        
-        # load initial velocity -> momentum today
-        u0 = np.load(f'neutrino_vectors/nu_{int(Nr+1)}.npy')[0][3:6]
-        p0 = np.sqrt(np.sum(u0**2)) * m_nu_kg.value
-        p0s[Nr] = p0
+    n_nus = np.load('neutrino_data/number_densities.npy')
 
-        # load "last" velocity -> momentum at z_back
-        u_back = np.load(f'neutrino_vectors/nu_{int(Nr+1)}.npy')[-1][3:6]
-        p_back = np.sqrt(np.sum(u_back**2)) * m_nu_kg.value
-        p_backs[Nr] = p_back
-
-
-    #NOTE: Attach units [kg*kpc/s] to p0s and p_backs.
-    p_unit = unit.kg*unit.kpc/unit.s
-    p0s, p_backs = p0s*p_unit, p_backs*p_unit
-
-    n_nu = fct.number_density(p0s, p_backs, m_nu)
-    print('Final number density:', n_nu)
+    plt.loglog(mass_range_eV, n_nus)
+    plt.xlabel('neutrino mass [eV]')
+    plt.ylabel(r'number density [1/$cm^3$]')
+    plt.show()

shared/functions.py
@@ -1,6 +1,6 @@
-from astropy.units import equivalencies
 from shared.preface import *
 import shared.my_units as my
+import shared.control_center as CC
 
 def rho_NFW(r, rho_0, r_s):
     """NFW density profile.
@@ -212,11 +212,11 @@ def dPsi_dxi_NFW(x_i, z, rho_0, M_vir):
     return derivative_vector.to(unit.kpc/unit.s**2)
 
 
-def Fermi_Dirac(p, m_nu):
+def Fermi_Dirac(p):
     """Fermi-Dirac phase-space distribution for CNB neutrinos. 
     
     Zero chem. potential and temp. T_nu (CNB temp. today). This distribution
-    is for relativistic neutrinos
+    is for relativistic neutrinos #? so no mass?
 
     Args:
         p (array): magnitude of momentum, must be in eV!
@@ -226,16 +226,18 @@ def Fermi_Dirac(p, m_nu):
         array: Value of Fermi-Dirac distr. at p.
     """
 
-    # Plug into Fermi-Dirac distribution
-    m_nu = 0.*unit.eV  #? not sure if we need mass
-    T_in_eV = my.T_nu.to(unit.eV, unit.temperature_energy())
-    arg_of_exp = np.sqrt(p**2+m_nu**2)/T_in_eV
+    
+    #NOTE: Temp. at z_back is higher than T_nu today.
+    T_zback_eV = my.T_nu.to(unit.eV, unit.temperature_energy())*(1+CC.Z_STOP)
+
+    # Plug into Fermi-Dirac distribution 
+    arg_of_exp = np.sqrt(p**2)/T_zback_eV
     f_of_p = 1 / (np.exp(arg_of_exp.value) + 1)
 
     return f_of_p
 
 
-def number_density(p0, p_back, m_nu):
+def number_density(p0, p_back):
     """Neutrino number density obtained by integration over initial momenta.
 
     Args:
@@ -261,7 +263,7 @@ def number_density(p0, p_back, m_nu):
 
     # precomputed factors
     const = g/(2*np.pi**2)
-    FDvals = Fermi_Dirac(p_back_sort, m_nu)
+    FDvals = Fermi_Dirac(p_back_sort)
 
     #NOTE: n ~ integral dp p**2 f(p), the units come from dp p**2, which have
     #NOTE: eV*3 = 1/eV**-3 ~ 1/length**3