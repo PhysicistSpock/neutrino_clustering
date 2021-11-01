008f378b1860fec89258fc585134c63f114ba926
Fabian Zimmer <fabiantje.zimmertje@gmail.com>
2021-11-01 08:02:06 +0100

    ~20k particles with halo

--- shared/control_center.py
+++ shared/control_center.py
@@ -1,16 +1,16 @@
 from shared.preface import *
 
 
-PHIs = 2
-THETAs = 2
-Vs = 2
-NR_OF_NEUTRINOS = PHIs*THETAs*Vs  # 19600
+PHIs = 10
+THETAs = 10
+Vs = 100
+NR_OF_NEUTRINOS = PHIs*THETAs*Vs  # 10000
 NU_MASS = 0.05*unit.eV
 NU_MASS_KG = NU_MASS.to(unit.kg, unit.mass_energy())
 N0 = 112  # standard neutrino number density in [1/cm**3]
 
 # Redshift integration parameters
-Z_START, Z_STOP, Z_AMOUNT = 0., 4., 99
+Z_START, Z_STOP, Z_AMOUNT = 0., 4., 199
 Z_START_LOG = 1e-1
 zeds_pre = np.geomspace(Z_START_LOG, Z_STOP, Z_AMOUNT) - Z_START_LOG
 ZEDS = np.insert(zeds_pre, len(zeds_pre), 4.)
