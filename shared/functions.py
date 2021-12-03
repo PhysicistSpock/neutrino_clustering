3ee18fffa4d4fb68d71bbc987beb9aa2746cc7d0
Fabian Zimmer <fabiantje.zimmertje@gmail.com>
2021-12-03 14:05:23 +0100

    sync

--- shared/functions.py
+++ shared/functions.py
@@ -265,7 +265,7 @@ def dPsi_dxi_NFW(x_i, z, rho_0, M_vir):
     r = np.sqrt(np.sum(x_i**2.))
     
     # Implement constant density core below certain radius.
-    R_core = r_vir / 100  # so 3 kpc at z=0, then shrinks as z increases.
+    R_core = r_vir 
     if r <= R_core:
         r = R_core
 
---
+++ sims.py
@@ -0,0 +1,7 @@
+from shared.preface import *
+import shared.my_units as my
+import shared.functions as fct
+import shared.control_center as CC
+
+
+data = load('/home/fabian/ownCloud/sim_data/snapshot_0036.hdf5')
