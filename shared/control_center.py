7a7cb67f2b134ffcd71cbeba14b10d31dd193b58
Fabian Zimmer <fabiantje.zimmertje@gmail.com>
2021-12-14 12:25:06 +0100

    s

--- shared/control_center.py
+++ shared/control_center.py
@@ -16,15 +16,8 @@ NU_MASS_KG_UPP = NU_MASS_UPP.to(unit.kg, unit.mass_energy())
 
 N0 = 112  # standard neutrino number density in [1/cm**3]
 
-## Redshift integration parameters
-
-#NOTE: OLD, logspaced
-# Z_START, Z_STOP, Z_AMOUNT = 0., 4., 199
-# Z_START_LOG = 1e-1
-# zeds_pre = np.geomspace(Z_START_LOG, Z_STOP, Z_AMOUNT) - Z_START_LOG
-# ZEDS = np.insert(zeds_pre, len(zeds_pre), 4.)
-
-#NOTE: NEW, linear spaced, denser for late times (closer to today)
+# Redshift integration parameters
+#NOTE: Linearly spaced, denser for late times (closer to today)
 late_steps = 200
 early_steps = 100
 Z_START, Z_STOP, Z_AMOUNT = 0., 4., late_steps+early_steps
@@ -35,4 +28,4 @@ ZEDS = np.concatenate((z_late, z_early))
 # Control if simulation runs forwards (+1) or backwards (-1) in time. 
 TIME_FLOW = -1
 
-SOLVER = 'RK23'  #! run sim with 'RK45' like they did
+SOLVER = 'RK23'
